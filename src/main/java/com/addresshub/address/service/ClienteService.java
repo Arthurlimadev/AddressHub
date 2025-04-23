0ffd03f6c4ce2e9852b80617e68de3c8e640f8bc
Arthur Lima <arthurlimadev98@gmail.com>
2025-04-23 15:46:59 -0300

    removing

--- src/main/java/com/addresshub/address/service/ClienteService.java
+++ src/main/java/com/addresshub/address/service/ClienteService.java
@@ -5,9 +5,13 @@ import com.addresshub.address.model.Cliente;
 public interface ClienteService {
 
     Iterable<Cliente> buscarTodos();
+
     Cliente buscarPorId(Long id);
+
     void inserir(Cliente cliente);
+
     void atualizar(Long id, Cliente cliente);
+
     void delete(Long id);
 
 }
